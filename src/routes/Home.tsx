a5ac65a00139a5c8b88402e7b990baf4075d6b60
songforthemuto <huni0423@naver.com>
2022-06-25 03:15:22 +0900

    todo input form

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 const Home = () => {
-    return <div>Home</div>;
+    const [text, setText] = useState("");
+
+    const _onChange = (e: ChangeEvent) => {
+        const { value } = e.target as HTMLInputElement;
+        setText(value);
+    };
+
+    const _onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        setText("");
+    };
+
+    return (
+        <>
+            <h1>To Do</h1>
+            <form onSubmit={_onSubmit}>
+                <input type="text" value={text} onChange={_onChange} />
+                <button>Add</button>
+            </form>
+            <ul></ul>
+        </>
+    );
 };
 
 export default Home;
