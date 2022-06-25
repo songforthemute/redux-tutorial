9658c67023ed4c591d5d2c05251858f43e52c75c
songforthemuto <huni0423@naver.com>
2022-06-26 03:38:32 +0900

    Detail Pages

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,5 +1,30 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { ToDosType, ToDoType } from "store";
+
+interface IdType {
+    id?: string;
+}
+
 const Detail = () => {
-    return <div>Detail</div>;
+    const { id }: IdType = useParams();
+    const toDos = useSelector((state: ToDosType) => state);
+    const toDo: ToDoType | undefined = toDos.find(
+        (e: ToDoType) => e.id === Number(id)
+    );
+
+    const dateConvertor = (d: number) => {
+        const createdAt = new Date(d).toLocaleString("ko-KR");
+        return createdAt;
+    };
+
+    return (
+        <>
+            <h1>Title : {toDo?.text}</h1>
+            {/* <h3>{toDo?.detail}</h3> */}
+            <h5>Created At : {dateConvertor(Number(id))}</h5>
+        </>
+    );
 };
 
 export default Detail;
